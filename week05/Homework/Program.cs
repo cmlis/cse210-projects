a631b1e37a59abc18bbcc21a4ebed9e6231731fd
Camila Santana <cssantana8@gmail.com>
2025-10-01 21:43:24 -0600

    W05 Inheritance DONE

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+    
+        Assignment a1 = new Assignment("Camila Santana", "Classes");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+    
+        MathAssignment a2 = new MathAssignment("Miguel Santos", "Addition", "2", "3");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("Thailis Gonzalez", "Public Health", "COVID19");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
